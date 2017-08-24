ae1a1d0c3234bdb607422916073f2358aa347cec
rogerh2 <rogeh2@gmail.com>
2017-08-24 19:08:28 -0400

    Revert "Fixed lab team top of page bug"
    This reverts commit 59a0aad7f56538055fe9f0401c77328f8b34b545.

--- wp-content/plugins/print_lab_team.php
+++ wp-content/plugins/print_lab_team.php
@@ -6,7 +6,7 @@ Plugin Name: Lab Team
 
 Plugin URI: http://www.yoursite.com/pure-awesomeness
 
-Description: This plugin makes a list of links to your lab team member's accounts
+Description: A Plugin that registers your lab teams!
 
 Version: 1.0
 
@@ -19,18 +19,17 @@ License: GPLv3
 */
 
 function print_lab_team(){
-	$profile_id = um_profile_id(); //This gets the ultimate members profile ID of the lab you're viewing
+	$profile_id = um_profile_id();
 	$single = false;
 	$key = 'lab_team';
-	$lab_team_list = get_user_meta( $profile_id, $key, $single); //This is a Wordpress function to get their data
+	$lab_team_list = get_user_meta( $profile_id, $key, $single);
 	$profile_lab_name = get_user_meta( $profile_id, 'lab_name', $single);
 	$lab_team_array = explode(',', $lab_team_list[0]);
-	$text_array = array();
 	if (!empty($lab_team_array[0])) {
-		array_push($text_array, '<div class="um-field-label">');
-		array_push($text_array, '<label>Lab Members</label>');
-		array_push($text_array, '<div class="um-clear"></div>');
-		array_push($text_array,  '</div>');
+		echo '<div class="um-field-label">';
+		echo '<label>Lab Members</label>';
+		echo '<div class="um-clear"></div>';
+		echo '</div>';
 	}
 	foreach($lab_team_array as $lab_team_member){
 		$user = get_user_by('login',$lab_team_member);
@@ -39,16 +38,13 @@ function print_lab_team(){
 			$user_first_name = $user->get('first_name');
 			$user_last_name = $user->get('last_name');
 			$user_lab_name = get_user_meta( $user_id, 'lab_name', $single);
-			$user_lab_role = get_user_meta( $user_id, 'lab_role', $single);
 			if($user_last_name){
 				if($user_lab_name == $profile_lab_name){
-					array_push($text_array, '<a href="../../user/'.$user_id.'">'.$user_first_name.' '.$user_last_name.'</a>'.' • '.$user_lab_role[0].'<br>');
+					echo '<a href="../../user/'.$user_id.'">'.$user_first_name.' '.$user_last_name.'</a><br>';
 				}
 			}
 		} 
 	}
-	//array_push($text_array, '</br>');
-	$final_html_list = implode("\n", $text_array);
-	return $final_html_list; //Do not echo information because it will go to the top of the page
+	echo '</br>';
 }
 ?>
